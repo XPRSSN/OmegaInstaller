81e339febb948bdbe7979be9609be9ccf11375f7
XPRSSN <78278246+XPRSSN@users.noreply.github.com>
2022-09-01 10:28:46 +0300

    Add files via upload

---
+++ OmegaInstaller/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace OmegaInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.DownloadDll = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(140, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(500, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Omega Installer";
+            // 
+            // DownloadDll
+            // 
+            this.DownloadDll.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.DownloadDll.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DownloadDll.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.DownloadDll.Location = new System.Drawing.Point(153, 113);
+            this.DownloadDll.Name = "DownloadDll";
+            this.DownloadDll.Size = new System.Drawing.Size(242, 188);
+            this.DownloadDll.TabIndex = 1;
+            this.DownloadDll.Text = "Download\r\nOmega DLL\r\n";
+            this.DownloadDll.UseVisualStyleBackColor = false;
+            this.DownloadDll.Click += new System.EventHandler(this.DownloadDll_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(401, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(239, 188);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Download\r\nInjector\r\n";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 375);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(454, 66);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Version: 4.57\r\nLast updated: 16th August 2022";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.DownloadDll);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button DownloadDll;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ OmegaInstaller/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace OmegaInstaller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DownloadDll_Click(object sender, EventArgs e)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile("https://raw.githubusercontent.com/XPRSSN/Omega/main/DLL/Omega16.8.2022.dll", "Omega31.8.2022.dll");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            using (var client = new WebClient())
+            {
+                client.DownloadFile("https://raw.githubusercontent.com/XPRSSN/Omega/main/Injectors/CSGhost-v4.3.1.exe", "CSGhost-v4.3.1.exe");
+            }
+
+        }
+
+    }
+}
